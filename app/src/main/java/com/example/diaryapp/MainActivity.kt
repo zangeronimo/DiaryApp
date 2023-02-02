edb4371188aea54a49c5dff142fabb9058553254
Zangeronimo <zangeronimo@gmail.com>
2023-02-02 16:03:06 -0300

    feat: Authentication Screen

--- app/src/main/java/com/example/diaryapp/MainActivity.kt
+++ app/src/main/java/com/example/diaryapp/MainActivity.kt
@@ -4,20 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.diaryapp.navigation.Screen
 import com.example.diaryapp.navigation.SetupNavGraph
 import com.example.diaryapp.ui.theme.DiaryAppTheme
+import com.example.diaryapp.util.Constants.APP_ID
+import io.realm.kotlin.mongodb.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             DiaryAppTheme {
                 val navController = rememberNavController()
-                SetupNavGraph(startDestination = Screen.Authentication.route, navController = navController)
+                SetupNavGraph(
+                    startDestination = getStartDestination(),
+                    navController = navController
+                )
             }
         }
     }
+}
+
+private fun getStartDestination(): String {
+    val user = App.create(APP_ID).currentUser
+    return if (user != null && user.loggedIn) Screen.Home.route
+    else Screen.Authentication.route
 }
--- app/src/main/java/com/example/diaryapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/diaryapp/navigation/NavGraph.kt
@@ -1,39 +1,112 @@
 package com.example.diaryapp.navigation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import com.example.diaryapp.util.Constants_example
+import com.example.diaryapp.presentation.screens.auth.AuthenticationScreen
+import com.example.diaryapp.presentation.screens.auth.AuthenticationViewModel
+import com.example.diaryapp.util.Constants
+import com.stevdzasan.messagebar.rememberMessageBarState
+import com.stevdzasan.onetap.rememberOneTapSignInState
+import io.realm.kotlin.mongodb.App
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 @Composable
 fun SetupNavGraph(startDestination: String, navController: NavHostController) {
     NavHost(startDestination = startDestination, navController = navController) {
-        authenticationRoute()
+        authenticationRoute(
+            navigateToHome = {
+                navController.popBackStack()
+                navController.navigate(Screen.Home.route)
+            }
+        )
         homeRoute()
         writeRoute()
     }
 }
 
-fun NavGraphBuilder.authenticationRoute() {
+@OptIn(ExperimentalMaterial3Api::class)
+fun NavGraphBuilder.authenticationRoute(
+    navigateToHome: () -> Unit
+) {
     composable(route = Screen.Authentication.route) {
+        val viewModel: AuthenticationViewModel = viewModel()
+        val authenticated by viewModel.autenticated
+        val loadingState by viewModel.loadingState
+        val oneTapState = rememberOneTapSignInState()
+        val messageBarState = rememberMessageBarState()
 
+        AuthenticationScreen(
+            authenticated,
+            loadingState,
+            oneTapState,
+            messageBarState,
+            onButtonClicked = {
+                oneTapState.open()
+                viewModel.setLoading(true)
+            },
+            onTokenIdReceived = { tokenId ->
+                viewModel.signInWithMongoAtlas(
+                    tokenId = tokenId,
+                    onSuccess = {
+                        messageBarState.addSuccess("Successfully Authenticated!")
+                        viewModel.setLoading(false)
+                    },
+                    onError = {
+                        messageBarState.addError(it)
+                        viewModel.setLoading(false)
+                    },
+                )
+            },
+            onDialogDismissed = { message ->
+                messageBarState.addError(Exception(message))
+                viewModel.setLoading(false)
+            },
+            navigateToHome,
+        )
     }
 }
 
 fun NavGraphBuilder.homeRoute() {
     composable(route = Screen.Home.route) {
-
+        val scope = rememberCoroutineScope()
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                scope.launch(Dispatchers.IO) {
+                    App.create(Constants.APP_ID).currentUser?.logOut()
+                }
+            }) {
+                Text(text = "Logout")
+            }
+        }
     }
 }
 
 fun NavGraphBuilder.writeRoute() {
     composable(
         route = Screen.Write.route,
-        arguments = listOf(navArgument(name = Constants_example.WRITE_SCREEN_ARGUMENT_KEY) {
+        arguments = listOf(navArgument(name = Constants.WRITE_SCREEN_ARGUMENT_KEY) {
             type = NavType.StringType
             nullable = true
             defaultValue = null
